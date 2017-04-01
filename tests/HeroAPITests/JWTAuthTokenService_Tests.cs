278554e0cfe07bed67ab224ebb895c2a47f20ae7
adamscarlat <adamscarlat@Adams-MBP-2.fios-router.home>
2017-04-01 17:47:49 -0400

    Fixing tests project- added tests

---
+++ tests/HeroAPITests/JWTAuthTokenService_Tests.cs
@@ -0,0 +1,20 @@
+using HeroAPI.Services;
+using Xunit;
+
+namespace tests.HeroAPITests
+{
+    public class JWTAuthTokenService_Tests
+    {
+        [Fact]
+        public void GetTokenExpirationDateTime_Tests()
+        {
+            var jwtService = new JWTAuthTokenServices();
+            var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJURVNUVVNFUiIsImp0aSI6IjYyZWJlNjEyLTRlZjQtNGE0YS1iNWNmLTNiMTI1MjU2MjgzYiIsImlhdCI6MTQ5MTA4Mjc1NSwiZXhwIjoxNDkxNzAxOTU1LCJpc3MiOiJFeGFtcGxlSXNzdWVyIiwiYXVkIjoiRXhhbXBsZUF1ZGllbmNlIn0.y3J7EkFg6bW04yvIAq1A_JXqJJzCZYew1ctbxJU2lQQ";
+            var expTime = jwtService.GetTokenExpirationDateTime(token);
+            ulong expectedExpTime = 1491701955;
+        
+            Assert.True(expTime == expectedExpTime);
+        }
+    }
+
+}
